7eff585da9a5c926ae5aaae1ce4f3b1c6af23aa2
Ben Vaillancourt <vaillancourtbenjamin484@gmail.com>
2026-03-23 19:18:12 -0400

    V0.0.1

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
 int main() {
-    system("ww2ogg.exe 3024720.wem --pcb packed_codebooks_aoTuV_603.bin");
+    system("./ww2ogg024/ww2ogg.exe 3024720.wem --pcb packed_codebooks_aoTuV_603.bin");
     return 0;
 }
